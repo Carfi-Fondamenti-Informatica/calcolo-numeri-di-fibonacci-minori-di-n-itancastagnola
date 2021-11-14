16e4a434f1c77e338ecd08aea91fcba8a4cfb7e1
itancastagnola <92515995+itancastagnola@users.noreply.github.com>
2021-11-14 23:53:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0, a=0, b=1, c=0;
+    cin>>n;
+
+    if (n<=0){
+        cout<< "errore"<<endl;
+
+    }else{
+        cout<<"1"<<endl;
+
+        for (int i=0; i<=n; i++){
+            c=a+b;
+            a=b;
+            b=c;
+            if (c<=n){
+                cout<<c<<endl;
+            }else{
+                break;
+            }
+        }
+    }
+
+    return 0;
 }
